7b45aecc763d1f7addc2fe2794069b5620d12f1c
dmy8100 <296602801@qq.com>
2025-09-17 23:09:28 +0800

    2

--- selfdrive/ui/qt/onroad/buttons.h
+++ selfdrive/ui/qt/onroad/buttons.h
@@ -9,7 +9,9 @@
 #endif
 
 const int btn_size = 192;
-const int img_size = (btn_size / 4) * 3;
+const int longitudinal_btn_size = 200; // 纵向控制模式切换按钮的独立尺寸
+const int img_size = (btn_size / 9) * 8;
+const int longitudinal_img_size = (btn_size / 8) * 8;
 
 class ExperimentalButton : public QPushButton {
   Q_OBJECT
@@ -31,6 +33,28 @@ protected:
   QPixmap experimental_img;
   bool experimental_mode;
   bool engageable;
+};
+
+class LongitudinalPersonalityButton : public QPushButton {
+  Q_OBJECT
+
+public:
+  explicit LongitudinalPersonalityButton(QWidget *parent = 0);
+  void updateState(const UIState &s);
+
+private:
+  void paintEvent(QPaintEvent *event) override;
+  void changeMode();
+
+  Params params;
+  int current_personality; // 0: Aggressive, 1: Standard, 2: Relaxed
+
+protected:
+  void drawButton(QPainter &p);
+
+  QPixmap aggressive_img;
+  QPixmap standard_img;
+  QPixmap relaxed_img;
 };
 
 void drawIcon(QPainter &p, const QPoint &center, const QPixmap &img, const QBrush &bg, float opacity);
